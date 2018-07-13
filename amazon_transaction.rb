b8020de4327f1e9cb1c328cd439032e2e4a2a5c0
Chrischelle Acelajado <chrischelleacelajado@gmail.com>
2018-07-13 17:45:08 +0000

    automatically backed up by learn

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -10,20 +10,20 @@ class AmazonTransaction
   end
 
   def add_item(title, price, quantity = 1)
-    @lastPrice = price
-    @total += price * quantity
+    @lastPrice = price      #lastPrice would  be the last price of the item
+    @total += price * quantity  # total would equal to the price and how much they got of that item
     for i in 0..quantity - 1
       @items << title
     end
   end
 
   def apply_discount
-    if @discount == 0
+    if @discount == 0       # if there is no discount to give 
       "There is no discount to apply."
     else
-      a = 100.0 - @discount
-      b = a / 100.0
-      @total = @total * b
+      a = 100.0 - @discount # minus the discount to the price?
+      b = a / 100.0         # divide the total of a to 100.00 to get next price
+      @total = @total * b   #multiply total to the discounted price
       "After the discount, the total comes to $#{@total.to_i}."
     end
   end
